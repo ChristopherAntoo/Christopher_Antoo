ed4fa9cf16e9a5301d8a79481942aee0f826a8ee
ChristopherAntoo <110451905+ChristopherAntoo@users.noreply.github.com>
2022-08-05 19:41:18 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,48 @@
+
+function htmlclick(vall)
+{
+    var vall="I coded this website by myself..."
+    
+    
+    document.getElementById("screen").value=vall
+    alert(vall)
+}
+
+function cssclick(vall)
+{
+    vall="I am good at CSS..."
+    
+    document.getElementById("screen").value=vall
+}
+
+function javascriptclick(vall)
+{
+    vall="I learned only Javascript basics..."
+    
+    document.getElementById("screen").value=vall
+}
+
+function pythonclick(vall)
+{
+    vall="My favourite programming language and excited to learn and code more on Python."
+    
+    document.getElementById("screen").value=vall
+}
+
+function htmlclick(vall)
+{
+    vall="I coded this website by myself..."
+    
+    document.getElementById("screen").value=vall
+}
+
+function htmlclick(vall)
+{
+    vall="I coded this website by myself..."
+    
+    document.getElementById("screen").value=vall
+}
+
+
+
+
